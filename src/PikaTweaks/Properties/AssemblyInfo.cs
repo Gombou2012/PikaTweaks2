0afebf0dd70324d9f40ff7d4faf748afdfda4bdc
Gombou2012 <120269442+Gombou2012@users.noreply.github.com>
2026-09-20 18:43:55 +0300

    Add files via upload

---
+++ src/PikaTweaks/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+[assembly: AssemblyTitle("PikaTweaks")]
+[assembly: AssemblyDescription("Windows optimizer based on the PikaTweaks V10.2 library")]
+[assembly: AssemblyCompany("PikaTweaks")]
+[assembly: AssemblyProduct("PikaTweaks")]
+[assembly: AssemblyVersion("11.0.0.0")]
+[assembly: AssemblyFileVersion("11.0.0.0")]
